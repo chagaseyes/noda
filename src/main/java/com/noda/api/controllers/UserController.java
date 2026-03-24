d1e1ed528b95edfba31354bdac891e5a8d33af82
Gabriel Chagas <chagasgabriel.gcv@gmail.com>
2026-03-24 20:23:48 -0300

    feat:implement UserController and verify JSON output

---
+++ src/main/java/com/noda/api/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.noda.api.controllers;
+
+import com.noda.api.models.User;
+import com.noda.api.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+}
+
+
+
